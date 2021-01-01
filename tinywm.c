2e3d47a48d7184f2e7e8187d7e69c7e5ca7b725c
liangyongxiang <tanekliang@gmail.com>
2021-01-01 14:09:55 +0800

    minor update

--- tinywm.c
+++ tinywm.c
@@ -24,35 +24,34 @@ int main()
     XGrabButton(dpy, 1, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
     XGrabButton(dpy, 3, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
 
-    for(;;)
-    {
+    for(;;) {
         XNextEvent(dpy, &ev);
         if(ev.type == KeyPress) {
-        	if (ev.xkey.subwindow == None) {
-				printf("key press, root: %ld, subwindow: None\n", root);
-			} else {
-            	XRaiseWindow(dpy, ev.xkey.subwindow);
-				printf("key press, root: %ld, subwindow: %ld, raise to top\n", root, ev.xkey.subwindow);
-			}
-		} else if(ev.type == ButtonPress) {
-        	if (ev.xkey.subwindow == None) {
-				printf("key press, root: %ld, subwindow: None\n", root);
-			} else {
-				printf("key press, root: %ld, subwindow: %ld\n", root, ev.xkey.subwindow);
-            	XGrabPointer(dpy, ev.xbutton.subwindow, True,
-            	        PointerMotionMask|ButtonReleaseMask, GrabModeAsync,
-            	        GrabModeAsync, None, None, CurrentTime);
-            	XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
-            	start = ev.xbutton;
-			}
+            if (ev.xkey.subwindow == None) {
+                printf("key press, root: %ld, subwindow: None\n", root);
+            } else {
+                XRaiseWindow(dpy, ev.xkey.subwindow);
+                printf("key press, root: %ld, subwindow: %ld, raise to top\n", root, ev.xkey.subwindow);
+            }
+        } else if(ev.type == ButtonPress) {
+            if (ev.xkey.subwindow == None) {
+                printf("key press, root: %ld, subwindow: None\n", root);
+            } else {
+                printf("key press, root: %ld, subwindow: %ld\n", root, ev.xkey.subwindow);
+                XGrabPointer(dpy, ev.xbutton.subwindow, True,
+                        PointerMotionMask|ButtonReleaseMask, GrabModeAsync,
+                        GrabModeAsync, None, None, CurrentTime);
+                XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
+                start = ev.xbutton;
+            }
         } else if(ev.type == MotionNotify) {
             int xdiff, ydiff;
-			int cnt = 0;
+            int cnt = 0;
 
             while(XCheckTypedEvent(dpy, MotionNotify, &ev)) {
-				cnt++;
-			}
-			printf("motion notify, loop: %d\n", cnt);
+                cnt++;
+            }
+            printf("motion notify, loop: %d\n", cnt);
 
             xdiff = ev.xbutton.x_root - start.x_root;
             ydiff = ev.xbutton.y_root - start.y_root;
@@ -62,8 +61,8 @@ int main()
                 MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
                 MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
         } else if(ev.type == ButtonRelease) {
-			printf("key release\n");
+            printf("button release\n");
             XUngrabPointer(dpy, CurrentTime);
-		}
+        }
     }
 }
